9c8dbef1368b7d2368b9460a178634c1b3fdce3e
davidmamani <>
2021-10-13 20:46:41 +0000

    programa con password

--- EstCiclicas/EstCiclicas.java
+++ EstCiclicas/EstCiclicas.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 import java.util.Random;
+import java.io.Console;
+
 public class EstCiclicas{  
     static Scanner sc=new Scanner(System.in);
     //para leer    valores del teclado
@@ -47,9 +49,28 @@ public class EstCiclicas{
     System.out.println("El faltorial de "+n+"  es:"+resulF);
   }
 
+  static void sumaParesRangoAB(){
+    //definir variables y datos de entrada
+    System.out.println("Ingrese valor  Inicial A:");
+    int a=sc.nextInt();
+    System.out.println("Ingrese valor final B:");
+    int b=sc.nextInt();
+    int suma=0;
+    //Proceso
+    for(int i=a; i<=b; i++){
+      if(i%2==0){
+          suma=suma+i;
+      }      
+    }
+    //datos de salida
+System.out.println("La suma de numeros pares es:"+suma);
+
+  }
+
   static void menuOpciones(){
     int opcion=0;
-    String msg="Eliga el algoritmo que desea probar\n 1=ssss\n2=Factorial";
+    String msg="Eliga el algoritmo que desea probar\n 1=ssss"+"\n2=Factorial"
+    +"\n4=Validar Password";
     System.out.println(msg);
     opcion=sc.nextInt();
 
@@ -57,13 +78,47 @@ public class EstCiclicas{
         switch(opcion){
             case 1: contarNumMenorCeroMayorCeroWhile(); break;
             case 2:  factorial(); break;
-           // case 3:  mesdelAnho(); break;
+            case 3:  sumaParesRangoAB(); break;
+            case 4:  validarContrasenha(); break;
             default: System.out.println("Opcion no existe!");
         }
       System.out.println(msg);
       opcion=sc.nextInt();    
     }
   }
+
+  static void validarContrasenha(){
+    //definicion de variables
+    String mensaje, password, usuario, msgrespuesta="";
+    int contador=1;
+    //datos de entrada
+    System.out.println("Ingrese su Usuario:");
+    usuario=sc.next();
+    Console console = System.console();
+    //Proceso
+    do{
+      //System.out.println("Ingrese su Password:");
+      //password=sc.next();        
+      char[] passwordArray = console.readPassword("Ingrese su Password:");  
+      if((String.valueOf(passwordArray)).equals("123456d")){
+        msgrespuesta="En hora buena";
+        System.out.println(msgrespuesta);  
+        //contador=4;
+        break;
+      }else if(contador<3){
+        msgrespuesta="lo siento password equivocado!";
+        System.out.println(msgrespuesta);        
+        // contador=contador+1;
+      }else{
+        msgrespuesta="oportunidades Agotadas";    
+        System.out.println(msgrespuesta);     
+      }
+    contador++;
+    }while(contador<=3);
+    //Datos de salida
+    //System.out.println(msgrespuesta);
+  }
+
 
   public static void main(String [] arg){
     menuOpciones();
