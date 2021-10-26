0854f9a74fa696f7872d72d26b363d1be672a4ea
davidmamani <>
2021-10-26 16:27:32 +0000

    Recursividad 2

--- SubProgramasUPeU/src/main/java/pe/edu/upeu/App.java
+++ SubProgramasUPeU/src/main/java/pe/edu/upeu/App.java
@@ -7,10 +7,13 @@ import pe.edu.upeu.modulo.*;
 public class App {
 
     static SubProgramas sp=new SubProgramas();
+    static Recursividad re=new Recursividad();
 
     public static void main( String[] args ){
         System.out.println( "Hello World!" );
         sp.saludo();
+        int valor=re.factorial(10);
+        System.out.println(valor);
     }
     
 }
