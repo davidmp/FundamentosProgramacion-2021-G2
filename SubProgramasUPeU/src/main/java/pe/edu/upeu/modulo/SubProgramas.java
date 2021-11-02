4fba3b9b4d1fafe36f736a3041ca5c4d1627baf1
davidmamani <>
2021-11-02 17:57:34 +0000

    Programacion concurrente y paralela

--- SubProgramasUPeU/src/main/java/pe/edu/upeu/modulo/SubProgramas.java
+++ SubProgramasUPeU/src/main/java/pe/edu/upeu/modulo/SubProgramas.java
@@ -1,12 +1,12 @@
 package pe.edu.upeu.modulo;
-import java.util.Scanner;
+
 
 public class SubProgramas{
   
   public String nombre="Dario";
   private String dni="";
 
-  public Scanner sc=new Scanner(System.in);
+
 
   public void saludo(){
     System.out.println("Hola "+nombre);
