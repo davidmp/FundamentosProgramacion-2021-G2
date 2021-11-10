0928ef8d3621079a4db8b03f6a8d4f6f84a6c1a1
davidmp <mamanipari@gmail.com>
2021-11-10 17:10:30 -0500

    Arreglos Introduccion

---
+++ SubProgramasUPeU/src/main/java/pe/edu/upeu/arreglos/ArreglosVM.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.arreglos;
+
+public class ArreglosVM {
+
+    public static void main(String[] args) {
+        String[] nombVect;
+        nombVect=new String[5];
+        nombVect[0]="Jhojan";
+
+        System.out.println(nombVect[0]);
+    }
+    
+}
