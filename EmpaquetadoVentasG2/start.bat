da06ee42e3a3a582981967b78e635166e242eab7
davidmp <mamanipari@gmail.com>
2021-12-07 13:12:07 -0500

    Colores Console

--- EmpaquetadoVentasG2/start.bat
+++ EmpaquetadoVentasG2/start.bat
@@ -1,1 +1,4 @@
+Start /wait "" "C:\Program Files\Java\jre1.8.0_301\bin\javacpl.exe"
+ECHO Opening JAVA Console
 java -jar ProyectoIntegradorVentasG2-1.0-SNAPSHOT.jar
+ECHO Opening JAVA Console
--- ProyectoIntegradorVentasG2/src/main/java/pe/edu/upeu/app/App.java
+++ ProyectoIntegradorVentasG2/src/main/java/pe/edu/upeu/app/App.java
@@ -1,12 +1,23 @@
 package pe.edu.upeu.app;
 
+import org.fusesource.jansi.Ansi;
+import org.fusesource.jansi.AnsiConsole;
+import org.fusesource.jansi.Ansi.Color;
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main( String[] args ) {
+        AnsiConsole.systemInstall();
+        Ansi color=new Ansi();
+        System.out.println(color.bgYellow().fg(Color.RED).a("Holassss"));
+        color=new Ansi();
+        //System.out.println(color.fg(Color.BLUE).a("Holassss"));
+        System.out.println(color.render("@|blue Holasss |@ @|green VErsdsd |@  "));
         MenuPrincipal mp=new MenuPrincipal();
         mp.mainLogin();
+        
     }
 }
